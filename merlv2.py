24720b9814c3660782c1de4d69d4e8beeb645b51
sjtrotter <stephen.j.trotter@gmail.com>
2022-12-16 16:56:11 +0000

    started v2

---
+++ merlv2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import os
+import tkinter as tk
+import shutil as sh
+import zipfile as zip
+# import win32api as win
+
+
+class Merl(tk.Frame):
+    pass
+
+root = tk.Tk()
+merl = Merl(root)
+root.mainloop()
